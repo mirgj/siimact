6248e51474625c5ddb138e6d6526c424d6620372
mirgj <mirash.gjolaj@gmail.com>
2018-01-25 18:10:39 +0800

    Test indentation

--- src/Alert.js
+++ src/Alert.js
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types';
 
 class Alert extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+	constructor(props) {
+		super(props);
+	}
 
-    render() {
+	render() {
 		const {
 			className,
 			color,
@@ -17,25 +17,25 @@ class Alert extends React.Component {
 			...attributes
 		} = this.props;
 
-        const classNames = mapClass({
-            ['siimple-alert']: true,
-            [`siimple-alert--${color}`]: true,
-            [className]: !!className
-        });
+		const classNames = mapClass({
+			['siimple-alert']: true,
+			[`siimple-alert--${color}`]: true,
+			[className]: !!className
+		});
 
-        return (
-        	<div className={classNames}
-	            {...attributes}>
-	            {close ? <div class="siimple-close" onClick={close}></div> : null }
-	            {children}
+		return (
+			<div className={classNames}
+			{...attributes}>
+			{close ? <div class="siimple-close" onClick={close}></div> : null }
+			{children}
 			</div>
-		);
-    }
+			);
+	}
 }
 
 Alert.defaultProps = {
-    color: 'navy',
-    close: null
+	color: 'navy',
+	close: null
 };
 Alert.propTypes = {
 	color: PropTypes.string,
