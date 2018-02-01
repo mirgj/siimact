844c524540ef136f84a11e7f142a9b8982d2ea85
mirgj <mirash.gjolaj@gmail.com>
2018-02-01 18:30:48 +0800

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import Alert from './Alert';
+import Content from './Content';
+import Quote from './Quote';
+import Code from './Code';
+import Paragraph from './Paragraph';
+import Heading from './Heading';
+import Link from './Link';
+import Small from './Small';
+import Tag from './Tag';
+import Button from './Button';
+import Spinner from './Spinner';
+import Tip from './Tip';
+import Table from './Table';
+import TRow from './TRow';
+import TCell from './TCell';
+import Box from './Box';
+import Footer from './Footer';
+import Jumbotron from './Jumbotron';
+import Breadcrumb from './Breadcrumb';
+import Crumb from './Crumb';
+import Menu from './Menu';
+import MenuGroup from './MenuGroup';
+import MenuItem from './MenuItem';
+import SubMenu from './SubMenu';
+import Tabs from './Tabs';
+import TabItem from './TabItem';
+import Navbar from './Navbar';
+import NavTitle from './NavTitle';
+import NavLink from './NavLink';
+import Grid from './Grid';
+import Row from './Row';
+import Col from './Col';
+import Form from './Form';
+import Field from './Field';
+import Input from './Input';
+import Textarea from './Textarea';
+import Select from './Select';
+import Label from './Label';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export {
+	Alert,
+	Content,
+	Quote,
+	Code,
+	Paragraph,
+	Heading,
+	Link,
+	Small,
+	Tag,
+	Button,
+	Spinner,
+	Tip,
+	Table,
+	TRow,
+	TCell,
+	Box,
+	Footer,
+	Jumbotron,
+	Breadcrumb,
+	Crumb,
+	Menu,
+	MenuGroup,
+	MenuItem,
+	SubMenu,
+	Tabs,
+	TabItem,
+	Navbar,
+	NavTitle,
+	NavLink,
+	Grid,
+	Row,
+	Col,
+	Form,
+	Field,
+	Input,
+	Textarea,
+	Select,
+	Label
+}
